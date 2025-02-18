9d77adbc55f67cea95b9fdaa0718d1a7a129ed58
Mattheus Guimarães <mattguimma@gmail.com>
2025-02-17 21:08:15 -0300

    f-commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const arakiDoodles = [
+    'img/diego.png',
+    'img/giorno.png',
+    'img/jolyne.png',
+    'img/kirajjl.png',
+    'img/yasuho.png',
+    'img/yukako.png',
+    'img/lucy.png',
+    'img/weather.png',
+    'img/bruno.png'
+];
+
+function sortSideBarBG() {
+    const i = Math.floor(Math.random() * arakiDoodles.length);
+    document.getElementById('sidebar').style.backgroundImage = `url(${arakiDoodles[i]})`;
+}
+
